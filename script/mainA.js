442458948b7ba985f3048adf27a4a02f9bbb3d36
domicmeia <71815610+domicmeia@users.noreply.github.com>
2021-11-30 21:30:31 +0900

    Update mainA.js

--- script/mainA.js
+++ script/mainA.js
@@ -6,11 +6,12 @@ var Timetable1 = function(){
     var remainShuttle = mainAT.filter(bus => bus.substring(0,2) > h || (bus.substring(0,2) == h && bus.substring(3,5) > m));
     var leftMinutes;
     if(remainShuttle[0]==null){
-        document.querySelector("#mainA").innerHTML="운행종료";
+        document.querySelector("#mainA").innerHTML="운행이 없습니다";
     }
     else{
         leftMinutes = (parseInt(remainShuttle[0].substring(0,2))*60 + parseInt(remainShuttle[0].substring(3,5))  - (parseInt(h) *60 + parseInt(m)));
-        document.querySelector("#mainA").innerHTML= parseInt(leftMinutes / 60) + " 시 " + leftMinutes % 60 +" 분";}
+        document.querySelector("#mainA").innerHTML= parseInt(leftMinutes / 60) + " 시 " + leftMinutes % 60 +" 분";
+    }
 }
 //정문
 
